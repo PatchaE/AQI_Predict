f00dd00497fb2f8b0e71dcbc17c9e1f38dff05d8
PatchaE <clickphone5538@gmail.com>
2025-04-27 01:54:24 +0700

    last commit final

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.post('/getAlldata_predict', async (req, res) => {
   try {
       
       const { date } = req.body; // รับค่าจาก Body
-      const result = await knex.select('*')
+      const result = await knex .select('*')
       .from('data_predict')
       
       res.set('Content-Type', 'application/json');
@@ -60,6 +60,48 @@ app.post('/data_predict', async (req, res) => {
       console.error('Error:', error);  // แสดงข้อผิดพลาดในคอนโซลของเซิร์ฟเวอร์
       res.status(500).json({ error: error.message });
   }
+});
+app.post('/insert_data', async (req, res) => {
+  try {
+      const { temp,hum,pm2_5,pm10,ozone,carbon,nitro,sulfur,people_no } = req.body; // รับค่าจาก Body
+      const result = await knex('sensor_data').insert({
+        temp: parseFloat(temp),
+        hum: parseFloat(hum),
+        pm2_5: parseFloat(pm2_5),
+        pm10: parseFloat(pm10),
+        ozone: parseFloat(ozone),
+        carbon: parseFloat(carbon),
+        nitro: parseFloat(nitro),
+        sulfur: parseFloat(sulfur),
+        people_no: Number(people_no),
+      });
+      //res.set('Content-Type', 'application/json');
+      res.json('Insert Success, ID:', result[0]);  // ส่งผลลัพธ์ไปยัง client
+  } catch (error) {
+      console.error('Error:', error);  // แสดงข้อผิดพลาดในคอนโซลของเซิร์ฟเวอร์
+      res.status(500).json({ error: error.message });
+  }
+});
+app.post('/insert_data_predict', async (req, res) => {
+  try {
+    const { predict_pm25,predict_pm10,predict_no2,predict_co,predict_so2,predict_o3,predict_aqi,predict_temp,predict_hum } = req.body; // รับค่าจาก Body
+    const result = await knex('sensor_data').insert({
+      predict_pm25: parseFloat(predict_pm25),
+      predict_pm10: parseFloat(predict_pm10),
+      predict_no2: parseFloat(predict_no2),
+      predict_co: parseFloat(predict_co),
+      predict_so2: parseFloat(predict_so2),
+      predict_o3: parseFloat(predict_o3),
+      predict_aqi: parseFloat(predict_aqi),
+      predict_temp: parseFloat(predict_temp),
+      predict_hum: parseFloat(predict_hum),
+    });
+    //res.set('Content-Type', 'application/json');
+    res.json('Insert Success, ID:', result[0]);  // ส่งผลลัพธ์ไปยัง client
+} catch (error) {
+    console.error('Error:', error);  // แสดงข้อผิดพลาดในคอนโซลของเซิร์ฟเวอร์
+    res.status(500).json({ error: error.message });
+}
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
